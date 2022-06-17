b79bca8236cd3e96263cae7a58cce2a08f67ede8
ProfSingletary <david.singletary@fscj.edu>
2022-06-17 15:42:45 -0400

    updates for SWC

---
+++ commands.bat
@@ -0,0 +1,7 @@
+REM https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html
+REM P:\>conda info --envs
+REM c:\Miniconda3\Scripts\activate swc22dataviz
+
+path=%PATH%;C:\Miniconda3;C:\Miniconda3\Scripts;
+conda info --envs
+activate swc22dataviz
