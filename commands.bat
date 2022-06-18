004c491073b12df5448a30657d6f8a37aa00dd60
ProfSingletary <david.singletary@fscj.edu>
2022-06-18 11:10:26 -0400

    removed output, updated setup

--- commands.bat
+++
@@ -1,7 +0,0 @@
-REM https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html
-REM P:\>conda info --envs
-REM c:\Miniconda3\Scripts\activate swc22dataviz
-
-path=%PATH%;C:\Miniconda3;C:\Miniconda3\Scripts;
-conda info --envs
-activate swc22dataviz
